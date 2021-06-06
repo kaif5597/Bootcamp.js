d1146e529a0ec3d7b1cf8fe3bd1ca8302e319436
kaif5597 <85429666+kaif5597@users.noreply.github.com>
2021-06-06 12:20:05 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h3>Hello ShapeAI</h3>
+      <br />
+      <p>
+        MY Name is Md.Musharraf and This was an astonishing bootcamp on
+        Javascript , React.js , HTML taken up by Shaurya Sinha.We covered
+        everything from Scratch .And I'm Looking forward to coming bootcamps.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
